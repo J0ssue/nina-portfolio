d5916a6d2ec2cb50e9b8f18cf47d2a1f50cc8f41
Josue Diaz <josue.diaz@filmworkz.com>
2023-02-27 13:19:41 +0000

    Feature: add new photos to albums and new events folder

--- src/configs/portfolioConfig.js
+++ src/configs/portfolioConfig.js
@@ -6,6 +6,10 @@ import brand5 from "../assets/images/portfolio/beach/IMG_8242.jpg";
 import brand6 from "../assets/images/portfolio/street/IMG_7567.jpg";
 import brand7 from "../assets/images/portfolio/beach/IMG_7565-3.jpg";
 import brand8 from "../assets/images/portfolio/beach/IMG_7616.jpg";
+import brand9 from "../assets/images/portfolio/brands/IMG_2536.jpg";
+import brand10 from "../assets/images/portfolio/brands/IMG_2550.jpg";
+import brand11 from "../assets/images/portfolio/brands/IMG_2560.jpg";
+import brand12 from "../assets/images/portfolio/brands/IMG_2582.jpg";
 
 import street0 from "../assets/images/portfolio/street/IMG_7002.jpg";
 import street1 from "../assets/images/portfolio/street/IMG_6969.jpg";
@@ -47,11 +51,43 @@ import portrait22 from "../assets/images/portfolio/portraits/IMG_8545.jpg";
 import portrait23 from "../assets/images/portfolio/portraits/IMG_8775-2.jpg";
 import portrait24 from "../assets/images/portfolio/portraits/IMG_8852.jpg";
 import portrait25 from "../assets/images/portfolio/portraits/g2.jpg";
-import portrait26 from "../assets/images/portfolio/portraits/IMG_9012.jpg"
- import portrait27 from "../assets/images/portfolio/portraits/IMG_9064.jpg"
- import portrait28 from "../assets/images/portfolio/portraits/IMG_9066-2.jpg"
+import portrait26 from "../assets/images/portfolio/portraits/IMG_9012.jpg";
+import portrait27 from "../assets/images/portfolio/portraits/IMG_9064.jpg";
+import portrait28 from "../assets/images/portfolio/portraits/IMG_9066-2.jpg";
+import portrait29 from "../assets/images/portfolio/portraits/IMG_1055.jpg";
+import portrait30 from "../assets/images/portfolio/portraits/IMG_1147-3.jpg";
+import portrait31 from "../assets/images/portfolio/portraits/IMG_1250.jpg";
+import portrait32 from "../assets/images/portfolio/portraits/IMG_1251.jpg";
+import portrait33 from "../assets/images/portfolio/portraits/IMG_1322.jpg";
+import portrait34 from "../assets/images/portfolio/portraits/IMG_1385.jpg";
+import portrait35 from "../assets/images/portfolio/portraits/IMG_1478.jpg";
+import portrait36 from "../assets/images/portfolio/portraits/IMG_1923-3.jpg";
+import portrait37 from "../assets/images/portfolio/portraits/IMG_1933-3.jpg";
+import portrait38 from "../assets/images/portfolio/portraits/IMG_1954.jpg";
+import portrait39 from "../assets/images/portfolio/portraits/IMG_1995.jpg";
+import portrait40 from "../assets/images/portfolio/portraits/IMG_2845.jpg";
+import portrait41 from "../assets/images/portfolio/portraits/IMG_2910.jpg";
+
+import events1 from "../assets/images/portfolio/events/IMG_0076.jpg";
+import events2 from "../assets/images/portfolio/events/IMG_0715.jpg";
+import events3 from "../assets/images/portfolio/events/IMG_0733.jpg";
+import events4 from "../assets/images/portfolio/events/IMG_0762.jpg";
+import events5 from "../assets/images/portfolio/events/IMG_2591.jpg";
+import events6 from "../assets/images/portfolio/events/IMG_2640.jpg";
+import events7 from "../assets/images/portfolio/events/IMG_2650-2.jpg";
+import events8 from "../assets/images/portfolio/events/IMG_2662-4.jpg";
+import events9 from "../assets/images/portfolio/events/IMG_2675.jpg";
+import events10 from "../assets/images/portfolio/events/IMG_2999.jpg";
+import events11 from "../assets/images/portfolio/events/IMG_3001.jpg";
+import events12 from "../assets/images/portfolio/events/IMG_3030.jpg";
+import events13 from "../assets/images/portfolio/events/IMG_3187.jpg";
+import events14 from "../assets/images/portfolio/events/IMG_3191.jpg";
 
 const brand = [
+  brand9,
+  brand10,
+  brand11,
+  brand12,
   brand7,
   brand6,
   brand8,
@@ -59,9 +95,22 @@ const brand = [
 ];
 
 const portraits = [
- portrait27,
-portrait26,
- portrait28,
+  portrait29,
+  portrait30,
+  portrait31,
+  portrait32,
+  portrait33,
+  portrait34,
+  portrait35,
+  portrait36,
+  portrait37,
+  portrait38,
+  portrait39,
+  portrait40,
+  portrait41,
+  portrait27,
+  portrait26,
+  portrait28,
   portrait1,
   portrait2,
   portrait3,
@@ -87,7 +136,7 @@ portrait26,
   portrait24,
   portrait25,
 ];
-const couples = [couples5,couples1, couples2, couples3, couples4];
+const couples = [couples5, couples1, couples2, couples3, couples4];
 const street = [
   street0,
   street1,
@@ -97,8 +146,24 @@ const street = [
   street7,
   street8,
   street10,
-brand2,
-brand4,
+  brand2,
+  brand4,
+];
+const events = [
+  events1,
+  events2,
+  events3,
+  events4,
+  events5,
+  events6,
+  events7,
+  events8,
+  events9,
+  events10,
+  events11,
+  events12,
+  events13,
+  events14,
 ];
 
 const portfolio = {
@@ -106,22 +171,33 @@ const portfolio = {
   buttonText: "view",
   projects: [
     {
+      title: "events",
+      message:
+        "In this album you will see some of my favorite events and the people I’ve worked with.",
+      cover: events[0],
+      gallery: events,
+      path: "/portfolio/events",
+    },
+    {
       title: "portraits",
-      message: "In this album you will see some of my favorite portraits of the beautiful faces I’ve worked eith so far. Mixed with a few photos from events.",
+      message:
+        "In this album you will see some of my favorite portraits of the beautiful faces I’ve worked with so far. Mixed with a few photos from events.",
       cover: portraits[0],
       gallery: portraits,
       path: "/portfolio/portraits",
     },
     {
       title: "couples",
-      message: "Here you can see my work with lovely couples. Don’t you just love love! I find that it inspires me a lot, working with people in love and capturing their emotions.",
+      message:
+        "Here you can see my work with lovely couples. Don’t you just love love! I find that it inspires me a lot, working with people in love and capturing their emotions.",
       cover: couples[0],
       gallery: couples,
       path: "/portfolio/couples",
     },
     {
       title: "street",
-      message: "This album is with street photography, mostly done for my own enjoyment.",
+      message:
+        "This album is with street photography, mostly done for my own enjoyment.",
       cover: street[8],
       gallery: street,
       path: "/portfolio/street",
